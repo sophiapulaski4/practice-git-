f1ebeed84ba7777683f4055264dcc1bf6417be80
caroline dugan <caroline.dugan@gmail.com>
2019-07-08 13:53:26 -0500

    first commit

---
+++ Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+//this is a comment! please use comment to help you keep track of your code!
+
+//Intention: I woud like to create a piece of code that would take each scholar's name and output each name into a string
+//Data Types involved: Strings
+//var name = "Sabyatha"
+//var name2 = "Nailah"
+//
+//
+//print ("Hi \(name)" + " and " + "\(name2)! We are so happy you're here at KWK Portland!")
+
+
+// Intention: working with integers + doubles
+// data types: Ints and Doubles
+//var int = 4
+//var double = 5.0
+//
+//print (Double(int) * double)
+var a = 12.4
+var b = 65.0
+var c = 31.0
+var d = 98
+
+
